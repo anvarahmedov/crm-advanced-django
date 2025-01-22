ed0ae0826d5a91e4da9a7b097d5b71e1ec8ccd3b
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-22 22:24:02 +0500

    updated models.py file added user and agent models

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
