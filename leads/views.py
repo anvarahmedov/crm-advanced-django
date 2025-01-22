46e2bf17d3b804b39cb70afff66ed20142512a44
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-23 03:26:19 +0500

    some changes

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from leads.models import Lead
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, "leads/lead_list.html", context=context)
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    context = {
+        "lead": lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
+
+def lead_create(request):
+    return render(request, 'leads/lead_create.html', context)
