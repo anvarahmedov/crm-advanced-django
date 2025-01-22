7ee3a858904229529088e04164815c267b8d5b56
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-22 23:16:41 +0500

    made migrations

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
@@ -8,8 +8,15 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
 
 
