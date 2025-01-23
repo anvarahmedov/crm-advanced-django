7d931defacd0803c8550f94b4c435dd903a968fd
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-24 00:47:39 +0500

    received data from post request

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from leads.models import Lead
+from leads.models import Agent, Lead
+from .forms import LeadForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -20,4 +21,14 @@ def lead_detail(request, pk):
     return render(request, 'leads/lead_detail.html', context)
 
 def lead_create(request):
+    #return HttpResponse(request.POST.get('first_name'))
+    form = LeadForm()
+    if (request.method == "POST"):
+        print("receiving post request")
+        form = LeadForm(request.POST)
+        Lead.objects.create(first_name = request.POST.get('first_name'), last_name=request.POST.get('last_name'),
+                            age=request.POST.get('age'), agent=Agent.objects.get(id=1))
+    context = {
+        "form": form
+    }
     return render(request, 'leads/lead_create.html', context)
