d8ed17865afeff98bd6f4bd0c86f49091ff458b4
anvarahmedov <boyoftokyo19@gmail.com>
2025-01-24 00:50:04 +0500

    confirmed if form is valid

--- leads/views.py
+++ leads/views.py
@@ -26,8 +26,11 @@ def lead_create(request):
     if (request.method == "POST"):
         print("receiving post request")
         form = LeadForm(request.POST)
-        Lead.objects.create(first_name = request.POST.get('first_name'), last_name=request.POST.get('last_name'),
+        if form.is_valid():
+            Lead.objects.create(first_name = request.POST.get('first_name'), last_name=request.POST.get('last_name'),
                             age=request.POST.get('age'), agent=Agent.objects.get(id=1))
+            print("the form is valid")
+            print(form.cleaned_data)
     context = {
         "form": form
     }
